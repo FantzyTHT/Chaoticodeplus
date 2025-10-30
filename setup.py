0ed55c3cf196730e41f499e16b2ebe3d04c3ad43
FantzyTHT <robertucciryan@gmail.com>
2025-10-30 15:23:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ChaoticodePlus",
+    name="Chaoticodeplus",
     version="0.1.0",
     description="CC+ Brainfuck-like interpreter with chaotic tokens",
     author="Your Name",
     packages=find_packages(),
     python_requires='>=3.8',
+
 )
